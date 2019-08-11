d3b94dff0a6d68d6830fa94e2cc466f83f443ee0
Callum Axon <callumaxon@gmail.com>
2019-08-11 17:46:16 +0100

    init graphql with basic work on ATC positions

---
+++ database/factories/PositionFactory.php
@@ -0,0 +1,15 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Position;
+use Faker\Generator as Faker;
+
+$factory->define(Position::class, function (Faker $faker) {
+    return [
+        'callsign' => strtoupper($faker->word).'_'.$faker->randomElement(['TWR', 'GND', 'DEL', 'APP', 'ATIS', 'CTR']),
+        'name' => ucfirst($faker->word).' '.$faker->randomElement(['Tower', 'Ground', 'Delivery', 'Approach', 'Information', 'Control']),
+        'frequency' => $faker->randomFloat(3, 0, 130),
+        'type' => $faker->numberBetween(1, 3),
+    ];
+});
---
+++ database/migrations/2019_08_11_162551_create_positions_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePositionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('positions', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('callsign');
+            $table->string('name');
+            $table->string('frequency');
+            $table->unsignedInteger('type');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('positions');
+    }
+}
