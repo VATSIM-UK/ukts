38d9bc31d015ab9ade096c33251ae05ed2cf76db
Callum Axon <callumaxon@gmail.com>
2019-09-23 19:08:43 +0100

    add tests for response class & use package test suite

--- app/AuthPackage/tests/Fixtures/MockJsonResponse.php
+++ app/AuthPackage/tests/Fixtures/MockJsonResponse.php
@@ -4,8 +4,28 @@ namespace VATSIMUK\Auth\Tests\Fixtures;
 
 class MockJsonResponse
 {
+    /**
+     * Represents a successfully retrieved user.
+     *
+     * @return mixed
+     */
     public static function successfulResponse()
     {
-        return json_decode('{"data":{"user":{"id":"1300005","name_first":"5th","name_last":"Test"}}}"');
+        return json_decode('{"data":{"user":{"id":"1300005","name_first":"5th","name_last":"Test"}}}');
+    }
+
+    /**
+     * Mock response for when a user is not found.
+     *
+     * @return mixed
+     */
+    public static function emptyResponse()
+    {
+        return json_decode('{"data":{"user":null}}');
+    }
+
+    public static function erroredResponse()
+    {
+        return json_decode('{"errors": [ { "message": "There was an error" } ]}');
     }
 }
--- app/AuthPackage/tests/ResponseClassTest.php
+++ app/AuthPackage/tests/ResponseClassTest.php
@@ -9,21 +9,60 @@ use VATSIMUK\Auth\Tests\Fixtures\MockJsonResponse;
 
 class ResponseClassTest extends TestCase
 {
-    protected $validResponse;
     protected $builder;
+    protected $normalResponse;
+    protected $erroredResponse;
+    protected $emptyDataResponse;
 
     protected function setUp(): void
     {
         parent::setUp();
 
         $this->builder = new PackageBuilder('user', ["name_first", "name_last",]);
-
-        $this->validResponse = new PackageResponse(MockJsonResponse::successfulResponse(), $this->builder);
+        $this->erroredResponse = new PackageResponse(MockJsonResponse::erroredResponse(), $this->builder);
+        $this->normalResponse = new PackageResponse(MockJsonResponse::successfulResponse(), $this->builder);
+        $this->emptyDataResponse = new PackageResponse(MockJsonResponse::emptyResponse(), $this->builder);
     }
 
     /** @test */
     public function itCorrectlyDetectsWhenErrorsAreInTheResponse()
     {
-        $this->assertFalse($this->validResponse->hasErrors());
+        $this->assertFalse($this->normalResponse->hasErrors());
+        $this->assertTrue($this->erroredResponse->hasErrors());
+    }
+
+    /** @test */
+    public function itCorrectlyDetectsWhenTheResponseIsEmpty()
+    {
+        $this->assertFalse($this->normalResponse->isEmpty());
+        $this->assertTrue($this->emptyDataResponse->isEmpty());
+    }
+
+    /** @test */
+    public function itHandlesEmptyResponseInGetResults()
+    {
+        $this->assertEquals($this->emptyDataResponse->getResults(), null);
+    }
+
+    /** @test */
+    public function itRetrievesFieldsCorrectlyFromNormalResponse()
+    {
+        $expected = [
+            'id' => 1300005,
+            'name_first' => '5th',
+            'name_last' => 'Test'
+        ];
+
+        $this->assertTrue($this->normalResponse->getResults() instanceof \stdClass);
+        $this->assertEquals((array)$this->normalResponse->getResults(), $expected);
+    }
+
+    /** @test */
+    public function itExtractsTheErrorsFromBadResponse()
+    {
+        $firstErrorIndex = 0;
+        $expected = ['message' => "There was an error"];
+
+        $this->assertEquals((array)$this->erroredResponse->getErrors()[$firstErrorIndex], $expected);
     }
 }
