df50042a8dc29a51eb1cdd65bd1d9852398acf4c
Callum Axon <callumaxon@gmail.com>
2019-12-28 14:19:52 +0000

    tidy up dockblock imports

--- app/Modules/Bookings/CreateBookingHandler.php
+++ app/Modules/Bookings/CreateBookingHandler.php
@@ -4,6 +4,7 @@ namespace App\Modules\Bookings;
 
 use App\Exceptions\OverlappingBookingException;
 use GraphQL\Type\Definition\ResolveInfo;
+use Illuminate\Contracts\Container\BindingResolutionException;
 use Nuwave\Lighthouse\Support\Contracts\GraphQLContext;
 
 class CreateBookingHandler
@@ -13,13 +14,13 @@ class CreateBookingHandler
      *
      * @param  null  $rootValue  Usually contains the result returned from the parent field. In this case, it is always `null`.
      * @param  mixed[]  $args  The arguments that were passed into the field.
-     * @param  \Nuwave\Lighthouse\Support\Contracts\GraphQLContext  $context  Arbitrary data that is shared between all fields of a single query.
-     * @param  \GraphQL\Type\Definition\ResolveInfo  $resolveInfo  Information about the query itself, such as the execution state, the field name, path to the field from the root, and more.
+     * @param  GraphQLContext  $context  Arbitrary data that is shared between all fields of a single query.
+     * @param  ResolveInfo  $resolveInfo  Information about the query itself, such as the execution state, the field name, path to the field from the root, and more.
      * @return mixed
      * @throws RatingRequirementNotMetException
      * @throws SpecialEndorsementNotAttainedException
      * @throws OverlappingBookingException
-     * @throws \Illuminate\Contracts\Container\BindingResolutionException
+     * @throws BindingResolutionException
      */
     public function __invoke($rootValue, array $args, GraphQLContext $context, ResolveInfo $resolveInfo)
     {
--- app/Modules/Bookings/UpdateBookingHandler.php
+++ app/Modules/Bookings/UpdateBookingHandler.php
@@ -13,8 +13,8 @@ class UpdateBookingHandler
      *
      * @param  null  $rootValue  Usually contains the result returned from the parent field. In this case, it is always `null`.
      * @param  mixed[]  $args  The arguments that were passed into the field.
-     * @param  \Nuwave\Lighthouse\Support\Contracts\GraphQLContext  $context  Arbitrary data that is shared between all fields of a single query.
-     * @param  \GraphQL\Type\Definition\ResolveInfo  $resolveInfo  Information about the query itself, such as the execution state, the field name, path to the field from the root, and more.
+     * @param  GraphQLContext  $context  Arbitrary data that is shared between all fields of a single query.
+     * @param  ResolveInfo  $resolveInfo  Information about the query itself, such as the execution state, the field name, path to the field from the root, and more.
      * @throws OverlappingBookingException
      */
     public function __invoke($rootValue, array $args, GraphQLContext $context, ResolveInfo $resolveInfo)
