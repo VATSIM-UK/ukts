5b72ebfe432bb488cae7ea46cbe1f0d633bd604e
Callum Axon <callumaxon@gmail.com>
2020-01-01 09:14:39 +0000

    reorganise booking feature test suite

--- tests/Feature/Booking/BookingCreateTest.php
+++ tests/Feature/Booking/BookingCreateTest.php
@@ -13,7 +13,7 @@ use Nuwave\Lighthouse\Testing\MakesGraphQLRequests;
 use Tests\TestCase;
 use Tests\Unit\Booking\BookingsTestHelper;
 
-class BookingTest extends TestCase
+class BookingCreateTest extends TestCase
 {
     use RefreshDatabase, MakesGraphQLRequests, BookingsTestHelper;
 
@@ -27,145 +27,6 @@ class BookingTest extends TestCase
         $this->mockUserFind();
 
         $this->position = factory(Position::class)->create();
-    }
-
-    /* @test */
-    public function testItReturnsBookingsPaginated()
-    {
-        $bookings = factory(Booking::class, 15)->create();
-
-        $data = $this->graphQL('
-        {
-            bookings (first: 10) {
-                data {
-                    id
-                    position {
-                      id
-                      callsign
-                    }
-                }
-                paginatorInfo {
-                    currentPage
-                    lastPage
-                }
-            }
-        }
-        ')->assertJsonStructure([
-            'data' => [
-                'bookings' => [
-                    'data' => [
-                        [
-                            'id',
-                            'position' => [
-                                'id',
-                                'callsign',
-                            ],
-                        ],
-                    ],
-                    'paginatorInfo' => [
-                        'currentPage',
-                        'lastPage',
-                    ],
-                ],
-            ],
-        ]);
-    }
-
-    /* @test */
-    public function testItReturnsBookingsForDate()
-    {
-        factory(Booking::class)->create([
-            'from' => '2019-08-10 14:00:00',
-            'to' => '2019-08-10 15:00:00',
-        ]);
-        factory(Booking::class)->create([
-            'from' => '2019-08-09 14:00:00',
-            'to' => '2019-08-09 15:00:00',
-        ]);
-        factory(Booking::class)->create([
-            'from' => '2019-08-09 20:00:00',
-            'to' => '2019-08-09 22:30:00',
-        ]);
-
-        $data = $this->graphQL('
-        {
-            bookingsByDate (from: "2019-08-09") {
-                id
-            }
-        }
-        ')->assertJson([
-            'data' => [
-                'bookingsByDate' => [
-                    ['id' => 2],
-                    ['id' => 3],
-                ],
-            ],
-        ]);
-    }
-
-    /* @test */
-    public function testItReturnsBookingsBetweenDates()
-    {
-        factory(Booking::class)->create([
-            'from' => '2019-08-06 14:00:00',
-            'to' => '2019-08-06 15:00:00',
-        ]);
-        factory(Booking::class)->create([
-            'from' => '2019-08-09 20:00:00',
-            'to' => '2019-08-09 22:30:00',
-        ]);
-        factory(Booking::class)->create([
-            'from' => '2019-08-04 14:00:00',
-            'to' => '2019-08-04 15:00:00',
-        ]);
-        factory(Booking::class)->create([
-            'from' => '2019-08-10 14:00:00',
-            'to' => '2019-08-10 15:00:00',
-        ]);
-
-        $data = $this->graphQL('
-        {
-            bookingsBetweenDates (from: {
-                from: "2019-08-05"
-                to: "2019-08-09"
-            }) {
-                id
-            }
-        }
-        ')->assertJson([
-            'data' => [
-                'bookingsBetweenDates' => [
-                    ['id' => 1],
-                    ['id' => 2],
-                ],
-            ],
-        ]);
-    }
-
-    /* @test */
-    public function testItReturnsBookingById()
-    {
-        $bookings = factory(Booking::class, 3)->create();
-
-        $this->graphQL("
-        {
-            booking(id: {$bookings->get(1)->id}) {
-                id
-                position {
-                    callsign
-                }
-            }
-        }
-        ")->assertJson([
-            'data' => [
-                'booking' => [
-                    'id' => $bookings->get(1)->id,
-                    'position' => [
-                        'callsign' => $bookings->get(1)->position->callsign,
-                    ],
-                ],
-            ],
-        ]);
     }
 
     /** @test */
@@ -207,7 +68,6 @@ class BookingTest extends TestCase
     /** @test */
     public function testInvalidBookingCantBeCreated()
     {
-
         // Invalid "from" date
         $this->graphQL("
           mutation {
@@ -419,36 +279,6 @@ class BookingTest extends TestCase
     }
 
     /** @test */
-    public function testItCanUpdateABooking()
-    {
-        factory(Booking::class)->create([
-            'id' => 1,
-            'user_id' => 1300001,
-            'position_id' => 1,
-            'from' => new Carbon('2019-08-20 15:00:00'),
-            'to' => new Carbon('2019-08-20 16:30:00'),
-        ]);
-
-        $this->graphQL('
-            mutation {
-                updateBooking(id: 1, to: "2019-08-20 17:00:00") {
-                    id
-                }
-            }')->assertJson([
-            'data' => [
-                'updateBooking' => [
-                    'id' => 1,
-                ],
-            ],
-        ]);
-
-        $this->assertDatabaseHas('bookings', [
-            'id' => 1,
-            'to' => new Carbon('2019-08-20 17:00:00'),
-        ]);
-    }
-
-    /** @test */
     public function testItCanDeleteABooking()
     {
         factory(Booking::class)->create();
@@ -473,7 +303,7 @@ class BookingTest extends TestCase
 
     /**
      * Setup a test case to pass the rating check for testing overlapping.
-     * @param string $callsign
+     * @param  string  $callsign
      */
     private function bypassRatingChecks($callsign = 'EGGD_TWR')
     {
---
+++ tests/Feature/Booking/BookingUpdateTest.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace Tests\Feature\Booking;
+
+use App\Modules\Bookings\Booking;
+use App\Position;
+use Carbon\Carbon;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Nuwave\Lighthouse\Testing\MakesGraphQLRequests;
+use Tests\TestCase;
+use Tests\Unit\Booking\BookingsTestHelper;
+
+class BookingUpdateTest extends TestCase
+{
+    use RefreshDatabase, MakesGraphQLRequests, BookingsTestHelper;
+
+    private $position;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $this->actingAs($this->mockedUser());
+        $this->mockUserFind();
+
+        $this->position = factory(Position::class)->create();
+    }
+
+
+    /** @test */
+    public function testItCanUpdateABooking()
+    {
+        factory(Booking::class)->create([
+            'id' => 1,
+            'user_id' => 1300001,
+            'position_id' => 1,
+            'from' => new Carbon('2019-08-20 15:00:00'),
+            'to' => new Carbon('2019-08-20 16:30:00'),
+        ]);
+
+        $this->graphQL('
+            mutation {
+                updateBooking(id: 1, from: "2019-08-20 17:00:00", to: "2019-08-20 18:00:00") {
+                    id
+                }
+            }')->assertJson([
+            'data' => [
+                'updateBooking' => [
+                    'id' => 1,
+                ],
+            ],
+        ]);
+
+        $this->assertDatabaseHas('bookings', [
+            'id' => 1,
+            'from' => new Carbon('2019-08-20 17:00:00'),
+            'to' => new Carbon('2019-08-20 18:00:00'),
+        ]);
+    }
+
+}
