1ec784360c11ee6b53ab73d8e555026060063f29
aaron <aaron@test.com>
2020-02-07 00:39:39 +0000

    add availability deletion

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -2,6 +2,7 @@
 
 namespace App\Console;
 
+use App\Modules\Availability\Availability;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
@@ -24,8 +25,9 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        // $schedule->command('inspire')
-        //          ->hourly();
+        $schedule->call(function () {
+            Availability::where('to', '<', Carbon::now()->delete();
+        })->dailyAt('0:00');
     }
 
     /**
