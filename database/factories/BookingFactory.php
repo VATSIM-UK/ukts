550545ac709b19328b6cbd571065c5ffb1b12335
Keanu73 <guestone63@gmail.com>
2020-05-01 14:54:12 +0100

    This is highly confusing..............

--- database/factories/BookingFactory.php
+++ database/factories/BookingFactory.php
@@ -8,12 +8,13 @@ use Faker\Generator as Faker;
 $factory->define(Booking::class, function (Faker $faker) {
     $from = $faker->dateTimeThisYear;
     $to = $faker->dateTimeInInterval($from, '+1 day');
+    $network_type = 0;
 
     return [
         'user_id' => $faker->numberBetween(1000000, 9999999),
         'position_id' => factory(\App\Modules\Position\Position::class),
-        'network_type' => 0,
         'from' => $from,
         'to' => $to,
+        'network_type' => $network_type,
     ];
 });
