c34728381b0975331948f70a7e0af95a1c8b2628
Callum Axon <callumaxon@gmail.com>
2020-05-12 13:08:51 +0100

    inertia install

---
+++ resources/js/app.js
@@ -0,0 +1,15 @@
+import { InertiaApp } from '@inertiajs/inertia-vue'
+import Vue from 'vue'
+
+Vue.use(InertiaApp)
+
+const app = document.getElementById('app')
+
+new Vue({
+  render: h => h(InertiaApp, {
+    props: {
+      initialPage: JSON.parse(app.dataset.page),
+      resolveComponent: name => require(`./Pages/${name}`).default,
+    },
+  }),
+}).$mount(app)
--- webpack.mix.js
+++ webpack.mix.js
@@ -11,5 +11,9 @@ const mix = require('laravel-mix');
  |
  */
 
-mix.js('resources/js/app.js', 'public/js')
-    .sass('resources/sass/app.scss', 'public/css');
+
+mix.js('resources/js/app.js', 'public/js').webpackConfig({
+    resolve: {
+        alias: { 'vue$': 'vue/dist/vue.runtime.js' }
+    }
+}).sass('resources/sass/app.scss', 'public/css');
